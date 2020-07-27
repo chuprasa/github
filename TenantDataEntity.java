0a28ec10d9994d5a07a1ad39a1b6c2c2e91407b2
chuprasa <chprasanna93@gmail.com>
2020-07-27 15:20:49 +0530

    cherry-pick

--- TenantDataEntity.java
+++ TenantDataEntity.java
@@ -2,6 +2,7 @@ package org.cisco.spade.jiraservice.dao.entity;
 
 public class TenantDataEntity {
 	
+<<<<<<< HEAD
 	private String name;
 	private String pasword;
 	public String getName() {
@@ -18,5 +19,22 @@ public class TenantDataEntity {
 	}
 	
 	
+=======
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+>>>>>>> b307d68... Create TenantDataEntity.java
 
 }
