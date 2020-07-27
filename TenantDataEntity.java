0f2ec4178fae3ffeee8123be34e8c99089b0fa26
chuprasa <chprasanna93@gmail.com>
2020-07-27 14:40:27 +0530

    Create TenantDataEntity.java

---
+++ TenantDataEntity.java
@@ -0,0 +1,22 @@
+package org.cisco.spade.jiraservice.dao.entity;
+
+public class TenantDataEntity {
+	
+	private String name;
+	private String pasword;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPasword() {
+		return pasword;
+	}
+	public void setPasword(String pasword) {
+		this.pasword = pasword;
+	}
+	
+	
+
+}
